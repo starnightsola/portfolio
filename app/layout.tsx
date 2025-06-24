5a11706f534899c1ea70d82d0e0659e6a7dd4d2b
HUANG <starnightsola@gmail.com>
2025-06-24 16:05:37 +0900

    エラー修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ReactNode } from 'react'
 import Header from '@/components/Header'
