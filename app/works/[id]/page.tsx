0ea4263d23ef4a9ce0f0df79a2c2f43cd95e871c
HUANG <starnightsola@gmail.com>
2025-06-25 11:56:39 +0900

    エラー修正

--- app/works/[id]/page.tsx
+++ app/works/[id]/page.tsx
@@ -1,12 +1,12 @@
-// app/works/[id]/page.tsx
 import { client } from '@/lib/client'
 
-type Props = {
+export default async function WorkDetailPage({
+  params,
+  searchParams,
+}: {
   params: { id: string }
-  searchParams: { draftKey?: string }
-}
-
-export default async function WorkDetailPage({ params, searchParams }: Props) {
+  searchParams?: { draftKey?: string }
+}) {
   const work = await client.getListDetail({
     endpoint: 'works',
     contentId: params.id,
